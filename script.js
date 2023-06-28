5f319ca93712e0b170efc7d9f2892cc2f6be984a
HanningtoneM <hanningtonepompyy@gmail.com>
2023-06-28 17:25:02 +0300

    Render myleadsfrom local storage

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+
 inputBtn.addEventListener("click", function () {
 	myLeads.push(inputEl.value);
     inputEl.value = ""
-
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 });
@@ -25,6 +32,7 @@ function renderLeads() {
 
     ulEl.innerHTML = listItems
 }
+
 
 
 
