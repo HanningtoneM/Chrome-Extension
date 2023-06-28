77a68289d24b965980607f147ff949fe1d3f61b5
HanningtoneM <hanningtonepompyy@gmail.com>
2023-06-28 17:43:54 +0300

    Add functionality to delete button

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const inputBtn = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads=[]
+  renderLeads()
+})
 
 
 inputBtn.addEventListener("click", function () {
