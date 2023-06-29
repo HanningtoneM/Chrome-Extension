0100c4856d41617a5e6dfc9b5d9a2a8bc694ce76
HanningtoneM <hanningtonepompyy@gmail.com>
2023-06-29 16:42:31 +0300

    Add tab button to js

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-const inputBtn = document.getElementById("input-btn");
 let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+	myLeads = leadsFromLocalStorage;
+	render(myLeads);
 }
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
 
-        listItems += `
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+	let listItems = "";
+	for (let i = 0; i < leads.length; i++) {
+		listItems += `
         <li>
            <a target='_blank' href="${leads[i]}">
             ${leads[i]}
            </a>
-        </li>`
-    }
+        </li>`;
+	}
 
-    ulEl.innerHTML = listItems
+	ulEl.innerHTML = listItems;
 }
 
-
 deleteBtn.addEventListener("dblclick", () => {
-  localStorage.clear()
-  myLeads=[]
-  render(myLeads)
-})
-
+	localStorage.clear();
+	myLeads = [];
+	render(myLeads);
+});
 
 inputBtn.addEventListener("click", () => {
 	myLeads.push(inputEl.value);
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+	inputEl.value = "";
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
+	render(myLeads);
 });
